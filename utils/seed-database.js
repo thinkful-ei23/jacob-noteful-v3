67785aa74de3d3204baf5760a7dc0901419c2fbf
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-08-22 12:24:29 -0400

    updated /utils/hash-password.js

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 
-const Folder = require('../models/folder');
 const Note = require('../models/note');
 const Tag = require('../models/tag');
+const Folder = require('../models/folder');
+const User = require('../models/user');
 
 const seedFolders = require('../db/seed/folders');
 const seedNotes = require('../db/seed/notes');
 const seedTags = require('../db/seed/tags');
+const seedUsers = require('../db/seed/users');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
@@ -19,12 +21,14 @@ mongoose.connect(MONGODB_URI)
       Note.insertMany(seedNotes),
       Folder.insertMany(seedFolders),
       Tag.insertMany(seedTags),
+      User.insertMany(seedUsers),
       Folder.createIndexes(),
-      Tag.createIndexes()
+      Tag.createIndexes(),
+      User.createIndexes()
     ]);
   })
   .then((results) => {
-    console.log(`Inserted ${results[0].length} Notes`,`Inserted ${results[1].length} Folders`, `Inserted ${results[2].length} Tags`);
+    console.log(`Inserted ${results[0].length} Notes`,`Inserted ${results[1].length} Folders`, `Inserted ${results[2].length} Tags`, `Inserted ${results[3].length} Users`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
